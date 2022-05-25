af63b2acd93c2814ea11d91e66b2203a318a80b4
bombard21 <wangxiaofeng153@gmail.com>
2022-05-25 17:10:28 +1200

    Update SeatPickerGUI.java

--- AirlineTicketBookingSystem/src/AirlineTicketBookingSystemGUI/SeatPickerGUI.java
+++ AirlineTicketBookingSystem/src/AirlineTicketBookingSystemGUI/SeatPickerGUI.java
@@ -4,10 +4,58 @@
  */
 package AirlineTicketBookingSystemGUI;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Xiaofeng Wang
  */
-public class SeatPickerGUI {
+public class SeatPickerGUI extends JFrame{
+    public JButton [][] seat;
+    private final DrawPanel drawPanel;
+    
+    
+    public SeatPickerGUI(){
+        drawPanel = new DrawPanel();
+        drawPanel.repaint();
+        
+        seat = new JButton[100][100];
+        for (int row = 0; row < seat.length; row++) {
+            for (int col = 0; col < seat[row].length; col++) {
+                seat[row][col] = new JButton();
+            }
+        }
+        super.add(drawPanel);
+        
+    }
+    
+    public class DrawPanel extends JPanel {
+        public DrawPanel() {
+            super.setPreferredSize(new Dimension(400, 400));
+        }
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            for(int row = 0; row < seat.length; row++) {
+                for(int col = 0; col < seat[row].length; col++) {
+                        seat[row][col].setBounds(row*1, col*30, 30, 30);
+                }
+            }
+        }
+    }
+    
+    public static void main (String[] args){
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SeatPickerGUI().setVisible(true);
+                
+            }
+        });
+    }
     
 }

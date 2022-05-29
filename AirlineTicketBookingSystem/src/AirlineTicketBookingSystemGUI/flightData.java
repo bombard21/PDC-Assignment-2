d95361bdf1a3a9e046519ef5818ce9c02aece5c2
bombard21 <wangxiaofeng153@gmail.com>
2022-05-29 23:07:58 +1200

    flight Data Code

--- AirlineTicketBookingSystem/src/AirlineTicketBookingSystemGUI/flightData.java
+++ AirlineTicketBookingSystem/src/AirlineTicketBookingSystemGUI/flightData.java
@@ -4,17 +4,26 @@
  */
 package AirlineTicketBookingSystemGUI;
 
+
 /**
  *
  * @author wangx
  */
 public class flightData extends javax.swing.JFrame {
 
+    private AirlineTicketBookingCode.Trip planeTrip;
+    private Booking booking;
     /**
      * Creates new form flightData
      */
-    public flightData() {
+    public flightData(AirlineTicketBookingCode.Trip planeTrip, Booking booking) {
+        this.planeTrip = planeTrip;
+        this.booking = booking;
         initComponents();
+    }
+
+    flightData() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     /**
@@ -27,21 +36,16 @@ public class flightData extends javax.swing.JFrame {
     private void initComponents() {
 
         planeidLabel = new javax.swing.JLabel();
-        planeID = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         dataTable = new javax.swing.JTable();
+        checkData = new javax.swing.JButton();
+        flightNumber = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         planeidLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         planeidLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         planeidLabel.setText("FlightNumber");
-
-        planeID.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                planeIDActionPerformed(evt);
-            }
-        });
 
         dataTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -68,6 +72,13 @@ public class flightData extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(dataTable);
 
+        checkData.setText("Check");
+        checkData.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkDataActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -76,9 +87,12 @@ public class flightData extends javax.swing.JFrame {
                 .addGap(21, 21, 21)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(planeidLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 152, Short.MAX_VALUE)
+                        .addComponent(planeidLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(planeID, javax.swing.GroupLayout.PREFERRED_SIZE, 373, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(flightNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(33, 33, 33)
+                        .addComponent(checkData)
+                        .addGap(34, 34, 34))
                     .addComponent(jScrollPane1))
                 .addContainerGap())
         );
@@ -88,8 +102,9 @@ public class flightData extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(planeidLabel)
-                    .addComponent(planeID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(26, 26, 26)
+                    .addComponent(checkData)
+                    .addComponent(flightNumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(24, 24, 24)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -97,11 +112,14 @@ public class flightData extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void planeIDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_planeIDActionPerformed
-        String plane = planeID.getText();
+    private void checkDataActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkDataActionPerformed
+        String flightData = this.flightNumber.getText();
         
-        
-    }//GEN-LAST:event_planeIDActionPerformed
+        if(evt.getSource() == checkData){
+            
+        }
+            
+    }//GEN-LAST:event_checkDataActionPerformed
 
     /**
      * @param args the command line arguments
@@ -139,9 +157,10 @@ public class flightData extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton checkData;
     private javax.swing.JTable dataTable;
+    private javax.swing.JTextField flightNumber;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField planeID;
     private javax.swing.JLabel planeidLabel;
     // End of variables declaration//GEN-END:variables
 }
